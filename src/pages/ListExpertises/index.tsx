249eb5c9da699648eb2fc9250b2177a754269ec8
Gustavo Rizzo S M de Albuquerque <grizzo.albu@gmail.com>
2026-06-24 16:07:36 -0300

    Hidding Expertise v1 and v2

--- src/pages/ListExpertises/index.tsx
+++ src/pages/ListExpertises/index.tsx
@@ -1,13 +1,7 @@
-import SectionExpertises from '../../components/SectionExpertises/SectionExpertises';
-import SectionExpertisesV2 from '../../components/SectionExpertisesV2/SectionExpertisesV2';
 import SectionExpertisesV3 from '../../components/SectionExpertisesV3/SectionExpertisesV3';
 
 export default function ListExpertises() {
     return (
-      <>
-        <SectionExpertises />
-        <SectionExpertisesV2 />
-        <SectionExpertisesV3 />
-      </>
+      <SectionExpertisesV3 />
     );
   }
